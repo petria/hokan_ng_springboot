c07cd5cd8a1929fba201ece76af595077afc2b76
Petri Airio <ext.petri.airio@landisgyr.com>
2015-03-27 08:52:41 +0200

    HeaderPanel homepage link
    Identification: -
    Change-Id: Ie4852a0a46b89631eb5d15c3918856ffdee162f7

--- hokan_ng_springboot-wicket/src/main/java/org/freakz/hokan_ng_springboot/bot/panel/HeaderPanel.java
+++ hokan_ng_springboot-wicket/src/main/java/org/freakz/hokan_ng_springboot/bot/panel/HeaderPanel.java
@@ -2,7 +2,9 @@ package org.freakz.hokan_ng_springboot.bot.panel;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.freakz.hokan_ng_springboot.bot.page.HokanBasePage;
 
 /**
  * Created by Petri Airio on 20.3.2015.
@@ -13,6 +15,7 @@ public class HeaderPanel extends Panel {
 
   public HeaderPanel(String id, WebPage page) {
     super(id);
+    add(new BookmarkablePageLink<Void>("homePageLink", HokanBasePage.class));
     add(new LoggedInUserPanel());
   }
 
