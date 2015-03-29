25f9603e8b274150a7e78c1e4440233b5df9a9be
Petri Airio <petri.j.airio@gmail.com>
2015-03-29 12:59:31 +0300

    UserEditor window

--- hokan_ng_springboot-wicket/src/main/java/org/freakz/hokan_ng_springboot/bot/panel/usereditor/UserEditorPanel.java
+++ hokan_ng_springboot-wicket/src/main/java/org/freakz/hokan_ng_springboot/bot/panel/usereditor/UserEditorPanel.java
@@ -1,7 +1,19 @@
 package org.freakz.hokan_ng_springboot.bot.panel.usereditor;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.freakz.hokan_ng_springboot.bot.jpa.entity.User;
+
 /**
  * Created by Petri Airio on 27.3.2015.
+ *
  */
-public class UserEditorPanel {
+public class UserEditorPanel extends Panel {
+
+  public UserEditorPanel(String id, final User user) {
+    super(id);
+    add(new Label("editUserLabel", user.toString()));
+
+  }
+
 }
