43950b8b4961bc09b1feca6595aa47ff9c6f8a4e
Petri Airio <petri.j.airio@gmail.com>
2015-04-29 07:42:24 +0300

    Google-api dependency

---
+++ hokan_ng_springboot-common/src/main/java/org/freakz/hokan_ng_springboot/bot/service/GoogleTranslatorService.java
@@ -0,0 +1,13 @@
+package org.freakz.hokan_ng_springboot.bot.service;
+
+
+/**
+ * Created by Petri Airio (petri.j.airio@gmail.com) on 29.4.2015.
+ *
+ */
+public class GoogleTranslatorService {
+
+  private static Translator translator;
+
+
+}
