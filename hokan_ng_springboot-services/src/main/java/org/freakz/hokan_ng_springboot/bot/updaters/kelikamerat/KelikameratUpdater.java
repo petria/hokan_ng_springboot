5ba8002af587a775d1fd1bec4767cf9a894a6043
Petri Airio <ext.petri.airio@landisgyr.com>
2015-06-22 15:24:55 +0300

    Kelikamera weather parse test
    Identification: -
    Change-Id: Ic6558c8acf23a9a26f00d2d78118d5fedded2857

--- hokan_ng_springboot-services/src/main/java/org/freakz/hokan_ng_springboot/bot/updaters/kelikamerat/KelikameratUpdater.java
+++ hokan_ng_springboot-services/src/main/java/org/freakz/hokan_ng_springboot/bot/updaters/kelikamerat/KelikameratUpdater.java
@@ -1,23 +1,29 @@
-package org.freakz.hokan_ng_springboot.bot.service;
+package org.freakz.hokan_ng_springboot.bot.updaters.kelikamerat;
 
+import lombok.extern.slf4j.Slf4j;
 import org.freakz.hokan_ng_springboot.bot.models.KelikameratUrl;
-import org.freakz.hokan_ng_springboot.bot.updaters.kelikamerat.KelikameratUpdater;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.junit.Test;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Petri Airio on 18.6.2015.
+ * Created by Petri Airio on 22.6.2015.
  *
  */
-public class KelikameratWeatherTest {
+@Component
+//@Scope("prototype")
+@Slf4j
+public class KelikameratUpdater {
 
-  String[] urls =
+  private static final String BASE_ULR = "http://www.kelikamerat.info";
+
+  private static final String[] KELIKAMERAT_URLS =
       {
           "http://www.kelikamerat.info/kelikamerat/Etel%C3%A4-Karjala",
           "http://www.kelikamerat.info/kelikamerat/Etel%C3%A4-Pohjanmaa",
@@ -39,32 +45,26 @@ public class KelikameratWeatherTest {
           "http://www.kelikamerat.info/kelikamerat/Varsinais-Suomi"
       };
 
-  @Test
-  public void testWeatherParse() throws IOException {
+  private List<KelikameratUrl> stationUrls = new ArrayList<>();
 
-    Document doc = Jsoup.connect("http://www.kelikamerat.info/kelikamerat/Varsinais-Suomi/Turku/tie-40/kt40_Ravattula").get();
-    Elements elements = doc.getElementsByClass("air-temp-on-image");
-    Element test = elements.get(0);
-    test = test.getElementById("air-temp-image");
-    String air = test.text();
-    int x = 0;
+  public void updateStations() throws IOException {
+    stationUrls.clear();
+    int urlCount = 0;
+    for (String url : KELIKAMERAT_URLS) {
+      Document doc = Jsoup.connect(url).get();
+      Elements elements = doc.getElementsByClass("road-camera");
+      for (int xx = 0 ; xx < elements.size(); xx++) {
+        Element div = elements.get(xx);
+        Element href =     div.child(0);
+        String hrefUrl = BASE_ULR + href.attributes().get("href");
+        KelikameratUrl kelikameratUrl = new KelikameratUrl(url, hrefUrl);
+        stationUrls.add(kelikameratUrl);
+        log.debug("{} url: {}", ++urlCount, hrefUrl);
+      }
+    }
   }
 
-  @Test
-  public void testParseLocations() throws IOException {
-    Document doc = Jsoup.connect("http://www.kelikamerat.info/kelikamerat/Etel%C3%A4-Savo").get();
-    Elements elements = doc.getElementsByClass("road-camera");
-    Element div = elements.get(0);
-    Element href =     div.child(0);
-    String url = href.baseUri() + href.attributes().get("href");
+  public List<KelikameratUrl> getStationUrls() {
+    return stationUrls;
   }
-
-  @Test
-  public void testUpdater() throws IOException {
-    KelikameratUpdater updater = new KelikameratUpdater();
-    updater.updateStations();
-    List<KelikameratUrl> urlList = updater.getStationUrls();
-    int x = 0;
-  }
-
 }
