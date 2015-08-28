c07fc9c4988089f9174cbdc0a12c8ff49696b26c
Petri Airio <petri.j.airio@gmail.com>
2015-08-28 07:33:53 +0300

    WebApp stuff to deploy in Tomcat

---
+++ hokan_ng_springboot-services/src/main/java/org/freakz/hokan_ng_springboot/bot/WebInitializer.java
@@ -0,0 +1,14 @@
+package org.freakz.hokan_ng_springboot.bot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HokanNgSpringBootServices.class);
+	}
+}
