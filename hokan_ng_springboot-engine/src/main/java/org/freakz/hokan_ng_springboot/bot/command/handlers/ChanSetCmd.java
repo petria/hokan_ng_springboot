4b36c9926c72cd7db9d1862b258aba55b0b9eeb0
Petri Airio <ext.petri.airio@landisgyr.com>
2015-10-20 10:11:13 +0300

    Re-factor channel resolving to base class to avoid duplicate code.
    Identification: -
    Change-Id: Ie892caa12a551499a7b4855906f17c066a301658

--- hokan_ng_springboot-engine/src/main/java/org/freakz/hokan_ng_springboot/bot/command/handlers/ChanSetCmd.java
+++ hokan_ng_springboot-engine/src/main/java/org/freakz/hokan_ng_springboot/bot/command/handlers/ChanSetCmd.java
@@ -1,14 +1,18 @@
 package org.freakz.hokan_ng_springboot.bot.command.handlers;
 
 import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.UnflaggedOption;
 import lombok.extern.slf4j.Slf4j;
 import org.freakz.hokan_ng_springboot.bot.command.HelpGroup;
 import org.freakz.hokan_ng_springboot.bot.command.annotation.HelpGroups;
 import org.freakz.hokan_ng_springboot.bot.events.EngineResponse;
 import org.freakz.hokan_ng_springboot.bot.events.InternalRequest;
 import org.freakz.hokan_ng_springboot.bot.exception.HokanException;
+import org.freakz.hokan_ng_springboot.bot.jpa.entity.Channel;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import static org.freakz.hokan_ng_springboot.bot.util.StaticStrings.ARG_CHANNEL_ID;
 
 /**
  * Created by Petri Airio on 16.10.2015.
@@ -25,11 +29,27 @@ public class ChanSetCmd extends Cmd {
   public ChanSetCmd() {
     super();
     setHelp("Sets Channel modes");
+
+    UnflaggedOption unflaggedOption = new UnflaggedOption(ARG_CHANNEL_ID)
+        .setRequired(false)
+        .setGreedy(false);
+    registerParameter(unflaggedOption);
+
+
   }
 
   @Override
   public void handleRequest(InternalRequest request, EngineResponse response, JSAPResult results) throws HokanException {
-    // TODO
+    String channelId = getChannelIdOrFail(ARG_CHANNEL_ID, request, response);
+    if (channelId == null) {
+      return;
+    }
+    Channel theChannel = getChannelOrFail(channelId, request, response);
+    if (theChannel == null) {
+      return;
+    }
+
   }
+
 
 }
--- hokan_ng_springboot-engine/src/main/java/org/freakz/hokan_ng_springboot/bot/command/handlers/Cmd.java
+++ hokan_ng_springboot-engine/src/main/java/org/freakz/hokan_ng_springboot/bot/command/handlers/Cmd.java
@@ -13,6 +13,7 @@ import org.freakz.hokan_ng_springboot.bot.exception.HokanEngineException;
 import org.freakz.hokan_ng_springboot.bot.exception.HokanException;
 import org.freakz.hokan_ng_springboot.bot.jms.JmsMessage;
 import org.freakz.hokan_ng_springboot.bot.jms.api.JmsSender;
+import org.freakz.hokan_ng_springboot.bot.jpa.entity.Channel;
 import org.freakz.hokan_ng_springboot.bot.jpa.service.*;
 import org.freakz.hokan_ng_springboot.bot.service.AccessControlService;
 import org.freakz.hokan_ng_springboot.bot.service.StatsService;
@@ -119,7 +120,6 @@ public abstract class Cmd implements HokkanCommand, CommandRunnable {
     }
   }
 
-
   public String getMatchPattern() {
     return String.format("!%s.*", getName().toLowerCase());
   }
@@ -132,6 +132,35 @@ public abstract class Cmd implements HokkanCommand, CommandRunnable {
     }
     return name;
   }
+
+  protected String getChannelIdOrFail(String channelId, InternalRequest request, EngineResponse response) {
+    if (request.getIrcEvent().isPrivate() && channelId == null) {
+      response.addResponse("ChannelID parameter is needed when using private message, try: !chanlist to get ID.");
+      return null;
+    }
+    return channelId;
+  }
+
+  protected Channel getChannelOrFail(String channelId, InternalRequest request, EngineResponse response) {
+    Channel theChannel = request.getChannel();
+    if (channelId != null) {
+      long id;
+      try {
+        id = Long.parseLong(channelId);
+      } catch (NumberFormatException ex) {
+        response.addResponse("Valid ChannelID parameter is needed, try: !chanlist");
+        return null;
+      }
+      theChannel = channelService.findOne(id);
+      if (theChannel == null) {
+        response.addResponse("No valid Channel found with id: %d, try: !chanlist to get ID.", id);
+        return null;
+      }
+    }
+
+    return theChannel;
+  }
+
 
   protected String buildSeeAlso(Cmd cmd) {
 
