375c182089884cce45e2334d41ac5e2b89ad155f
Petri Airio <ext.petri.airio@landisgyr.com>
2015-11-02 15:19:53 +0200

    try to get hostinfo in windows OS
    Identification: -
    Change-Id: I283c4479cdcae1fee8ddefd0bc03eed6f908eccb

--- hokan_ng_springboot-common/src/main/resources/hostinfo.bat
+++ hokan_ng_springboot-common/src/main/resources/hostinfo.bat
@@ -1,3 +1,11 @@
 @echo off
-echo "Some kind of Windows"
+rem echo "Some kind of Windows"
 rem c:\uptime.exe
+
+cd /D %TEMP%
+if exist systeminfo.txt (
+	type systeminfo.txt
+) else (
+	systeminfo >systeminfo.txt
+	type systeminfo.txt
+)
