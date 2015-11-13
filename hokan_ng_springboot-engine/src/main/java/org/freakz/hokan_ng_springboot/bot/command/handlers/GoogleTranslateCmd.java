e0117317cb7cca5601dc607fbb4ced78ff9c63b7
Petri Airio <ext.petri.airio@landisgyr.com>
2015-11-13 13:55:01 +0200

    Disable google translate command
    Identification: -
    Change-Id: I0f23c3cd97b5b4fac1cdd406aea051f65e338c11

--- hokan_ng_springboot-engine/src/main/java/org/freakz/hokan_ng_springboot/bot/command/handlers/GoogleTranslateCmd.java
+++ hokan_ng_springboot-engine/src/main/java/org/freakz/hokan_ng_springboot/bot/command/handlers/GoogleTranslateCmd.java
@@ -2,14 +2,11 @@ package org.freakz.hokan_ng_springboot.bot.command.handlers;
 
 import com.martiansoftware.jsap.JSAPResult;
 import com.martiansoftware.jsap.UnflaggedOption;
-import lombok.extern.slf4j.Slf4j;
 import org.freakz.hokan_ng_springboot.bot.events.EngineResponse;
 import org.freakz.hokan_ng_springboot.bot.events.InternalRequest;
 import org.freakz.hokan_ng_springboot.bot.events.ServiceRequestType;
 import org.freakz.hokan_ng_springboot.bot.events.ServiceResponse;
 import org.freakz.hokan_ng_springboot.bot.exception.HokanException;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 import static org.freakz.hokan_ng_springboot.bot.util.StaticStrings.ARG_TEXT;
 
@@ -17,9 +14,9 @@ import static org.freakz.hokan_ng_springboot.bot.util.StaticStrings.ARG_TEXT;
  * Created by Petri Airio (petri.j.airio@gmail.com) on 29.4.2015.
  *
  */
-@Component
-@Scope("prototype")
-@Slf4j
+//@Component
+//@Scope("prototype")
+//@Slf4j
 public class GoogleTranslateCmd extends Cmd {
 
 
