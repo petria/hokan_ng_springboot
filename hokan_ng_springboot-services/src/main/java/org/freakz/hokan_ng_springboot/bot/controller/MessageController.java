3a0d05005ce7eeaeeb855f7f19d12d3fadf832ed
Petri Airio <petri.j.airio@gmail.com>
2016-04-08 14:40:48 +0300

    Lunch WEB response
    Identification: -
    Change-Id: Ieb090f85b716229ded33d4b21e90b893c8576f34

--- hokan_ng_springboot-services/src/main/java/org/freakz/hokan_ng_springboot/bot/controller/MessageController.java
+++ hokan_ng_springboot-services/src/main/java/org/freakz/hokan_ng_springboot/bot/controller/MessageController.java
@@ -1,5 +1,12 @@
 package org.freakz.hokan_ng_springboot.bot.controller;
 
+import org.freakz.hokan_ng_springboot.bot.enums.LunchDay;
+import org.freakz.hokan_ng_springboot.bot.enums.LunchPlace;
+import org.freakz.hokan_ng_springboot.bot.models.LunchData;
+import org.freakz.hokan_ng_springboot.bot.models.LunchMenu;
+import org.freakz.hokan_ng_springboot.bot.service.lunch.LunchServiceImpl;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +19,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MessageController {
 
+  @Autowired
+  private LunchServiceImpl lunchService;
+
+
   @RequestMapping("/message")
   public String greeting(@RequestParam(value = "key", required = false) String key, Model model) {
     model.addAttribute("message", "tfufufufuf" + key);
     return "message";
   }
+
+  @RequestMapping("/lunch")
+  public String lunch(@RequestParam(value = "place", required = false) String placeKey, Model model) {
+    LunchPlace place = LunchPlace.LOUNAS_INFO_ENERGIA_KEIDAS;
+    if (placeKey != null) {
+      place = LunchPlace.getLunchPlace(placeKey);
+      if (place == null) {
+        model.addAttribute("place", "Unknown place: " + placeKey);
+        model.addAttribute("lunch", "");
+        return "lunch";
+      }
+    }
+    DateTime day = DateTime.now();
+    LunchData lunchData = lunchService.getLunchForDay(place, day);
+    LunchDay lunchDay = LunchDay.getFromDateTime(day);
+    LunchMenu lunchMenu = lunchData.getMenu().get(lunchDay);
+
+    model.addAttribute("place", place.getName());
+    model.addAttribute("lunch", lunchMenu.getMenu());
+    return "lunch";
+  }
+
 
 
 }
